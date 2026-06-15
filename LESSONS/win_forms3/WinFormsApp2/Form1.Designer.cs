9e480cfa0516df4455f891caca2baa7915e0f0b1
stupiqplonde <kovalevmihail38@gmail.com>
2026-06-15 09:13:27 +0300

    update

---
+++ LESSONS/win_forms3/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                components?.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            progressBar1 = new ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            pictureBoxCongratulations = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCongratulations).BeginInit();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 12);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(400, 23);
+            progressBar1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += Timer1_Tick;
+            // 
+            // pictureBoxCongratulations
+            // 
+            pictureBoxCongratulations.BorderStyle = BorderStyle.FixedSingle;
+            pictureBoxCongratulations.Location = new Point(12, 50);
+            pictureBoxCongratulations.Name = "pictureBoxCongratulations";
+            pictureBoxCongratulations.Size = new Size(400, 200);
+            pictureBoxCongratulations.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxCongratulations.TabIndex = 1;
+            pictureBoxCongratulations.TabStop = false;
+            pictureBoxCongratulations.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(424, 262);
+            Controls.Add(pictureBoxCongratulations);
+            Controls.Add(progressBar1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Прогресс";
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCongratulations).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBoxCongratulations;
+    }
+}
---
+++ LESSONS/win_forms3/WinFormsApp2/Form1.cs
@@ -0,0 +1,64 @@
+﻿namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void Timer1_Tick(object? sender, EventArgs e)
+        {
+            if (progressBar1.Value >= progressBar1.Maximum)
+            {
+                return;
+            }
+
+            progressBar1.Value++;
+
+            if (progressBar1.Value >= progressBar1.Maximum)
+            {
+                timer1.Stop();
+                ShowCongratulations();
+            }
+        }
+
+        private void ShowCongratulations()
+        {
+            pictureBoxCongratulations.Image?.Dispose();
+            pictureBoxCongratulations.Image = CreateCongratulationsImage();
+            pictureBoxCongratulations.Visible = true;
+        }
+
+        private static Bitmap CreateCongratulationsImage()
+        {
+            var bitmap = new Bitmap(380, 180);
+            using var g = Graphics.FromImage(bitmap);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.Clear(Color.FromArgb(255, 255, 215, 0));
+
+            using var borderPen = new Pen(Color.DarkGreen, 4);
+            g.DrawRectangle(borderPen, 2, 2, bitmap.Width - 5, bitmap.Height - 5);
+
+            using var textBrush = new SolidBrush(Color.DarkGreen);
+            using var font = new Font("Segoe UI", 32, FontStyle.Bold);
+            const string text = "Поздравляем!";
+            SizeF size = g.MeasureString(text, font);
+            g.DrawString(
+                text,
+                font,
+                textBrush,
+                (bitmap.Width - size.Width) / 2,
+                (bitmap.Height - size.Height) / 2);
+
+            return bitmap;
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            pictureBoxCongratulations.Image?.Dispose();
+            base.OnFormClosed(e);
+        }
+    }
+}
